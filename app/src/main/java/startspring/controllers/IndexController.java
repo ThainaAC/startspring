f08a621ba0e3aaade9f73cff670ff5f688a12d69
ThainaAC <alvesthaina568@gmail.com>
2023-05-10 17:49:21 +0000

    salvar

---
+++ app/src/main/java/startspring/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package startspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String home(@RequestParam("nome") String nome, Model model) {
+            model.addAttribute("nome", nome);
+            return "WEB-INF/index.jsp";
+ 
+    }
+}
